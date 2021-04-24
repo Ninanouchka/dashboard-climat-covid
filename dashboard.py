86a816096bb46c3a3477930aa038d151ed96c5cf
Ninanouchka <nina.varchavsky@cri-paris.org>
2021-04-24 12:05:26 +0200

    select station

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,4 @@
+##### IMPORTS
 import streamlit as st
 import pandas as pd
 from datetime import datetime
@@ -8,8 +9,11 @@ import matplotlib.pyplot as plt
 from dms2dec.dms_convert import dms2dec
 import pydeck as pdk
 import plotly.express as px
+import numpy as np 
+import pykrige.kriging_tools as kt 
+from pykrige.ok import OrdinaryKriging
 
-
+##### FUNCTIONS
 def geoloc_convert(loc_degree):
     """ Convert degrees minutes seconds coordinate to decimal latitude or longitude  """
     loc_decimal = dms2dec(loc_degree)
@@ -21,10 +25,31 @@ def scatter_map(df):
     """ Build scatter map with color gradient for iptcc """
     mark_size = [100 for i in df.index]
     fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_data=["iptcc"],
-                    color="iptcc", color_continuous_scale=['#7BD150', '#F6E626', '#FC9129', '#FF1B00', '#6E1E80'], size=mark_size, size_max=10, zoom=4, height=450)
+          color="iptcc", color_continuous_scale=['#7BD150', '#F6E626', '#F6E626', '#FC9129', '#FF1B00', '#6E1E80'], size=mark_size, size_max=10, zoom=4, height=450)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
+
+def heat_map(df):
+    """ Build heatmap with color gradient for iptcc """
+#     mark_size = [100 for i in df.index]
+    fig = px.density_mapbox(df, lat='latitude', lon='longitude', z='iptcc', radius=30, center=dict(lat=0, lon=180), zoom=4, height=450)
+#     fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_data=["iptcc"],
+#           color="iptcc", color_continuous_scale=['#7BD150', '#F6E626', '#F6E626', '#FC9129', '#FF1B00', '#6E1E80'], size=mark_size, size_max=10, zoom=4, )
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+def ok_map(df):
+    fig = plt.Figure(figsize=(12,7))
+    # Compute the ordinary kriging 
+    OK = OrdinaryKriging(df['latitude'], df['longitude'], df['iptcc'], variogram_model='linear', verbose=False, nlags=60, enable_plotting=True)
+    z, ss = OK.execute('points', df['latitude'], df['longitude']) 
+
+    df_gradient = df[['latitude', 'longitude']]
+    df_gradient['iptcc'] = pd.Series(z)
+    return scatter_map(df_gradient)
+    
 
 @st.cache
 def load_data():
@@ -46,6 +71,7 @@ df = load_data()
 min_ts = min(df["date"]).to_pydatetime()
 max_ts = max(df["date"]).to_pydatetime()
 
+##### SIDEBAR
 #slider to chose date
 st.sidebar.subheader("Inputs")
 day_date = pd.to_datetime(st.sidebar.slider("Date to chose", min_value=min_ts, max_value=max_ts, value=max_ts))
@@ -53,9 +79,11 @@ day_date = pd.to_datetime(st.sidebar.slider("Date to chose", min_value=min_ts, m
 # day = st.sidebar.text_input("Day", value='22')
 # month = st.sidebar.text_input("Month", value='04')
 # year = st.sidebar.text_input("Year", value='2021')
-show_heatmap = st.sidebar.checkbox("Show date range")
+show_timerange = st.sidebar.checkbox("Show date range")
+show_station = st.sidebar.selectbox("Stations", options= np.append([""], df['nom'].sort_values().unique()), index=0)
 
-if show_heatmap:
+
+if show_timerange:
     min_selection, max_selection = st.sidebar.slider("Timeline", min_value=min_ts, max_value=max_ts, value=[min_ts, max_ts])
 
     # Filter data for timeframe
@@ -64,13 +92,19 @@ if show_heatmap:
     st.write(f"Stations: {len(df)}")
 
 else:
-    
     # Get last day data 
 #     day_date = pd.to_datetime(year + month + day, format='%Y%m%d')
     st.write(f"Data for {day_date.date()}")
     df = df[(df["date"] == day_date)]
     st.write(f"Data Points: {len(df)}")
 
+if show_station != "":
+    st.write("hello.")
+    pass
+
+##### MAPS
 # Plot the stations on the map
 # st.map(df)
 st.plotly_chart(scatter_map(df), use_container_width=True)
+st.plotly_chart(heat_map(df), use_container_width=True)
+st.plotly_chart(ok_map(df), use_container_width=True)
